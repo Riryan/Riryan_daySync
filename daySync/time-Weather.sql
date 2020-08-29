c05b4d12465f56b0bcee540ef26310e02ab9fae1
Riryan <anthonyfmitchell@gmail.com>
2020-08-29 11:57:45 -0400

    Add files via upload

---
+++ daySync/time-Weather.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `daySync` (
+    `oldmin` int(11) NOT NULL, -- for a timer
+    `newmin` int(11) NOT NULL,
+    `weather` int (11) NOT NULL
+);
+
+ALTER TABLE `daySync`
+ADD PRIMARY KEY (`identifier`);
